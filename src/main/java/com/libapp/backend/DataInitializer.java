9208c6a9e266c344e0af828075347a50f5e2be40
thomasteinhoff <thomas11ventura@gmail.com>
2025-09-24 10:14:43 -0300

    build(main auth classes n files #1): structured repos, controllers & entities

---
+++ src/main/java/com/libapp/backend/DataInitializer.java
@@ -0,0 +1,5 @@
+package com.libapp.backend;
+
+public class DataInitializer {
+    
+}
---
+++ src/main/java/com/libapp/backend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.libapp.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.libapp.backend.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.username = :username")
+    Optional<User> findByUsername(String username);
+}
