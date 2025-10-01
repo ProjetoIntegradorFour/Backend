c41aa3483fe04be7e1777d2ae34b9c67a0069d7a
thomasteinhoff <thomas11ventura@gmail.com>
2025-09-30 21:57:17 -0300

    fix(test controller submition): forgot to include token handler in last commit :p

--- src/main/java/com/libapp/backend/controller/AuthController.java
+++ src/main/java/com/libapp/backend/controller/AuthController.java
@@ -1,12 +1,10 @@
 package com.libapp.backend.controller;
 
-import com.libapp.backend.entity.Role;
-import com.libapp.backend.entity.RoleName;
-import com.libapp.backend.entity.User;
-import com.libapp.backend.repository.RoleRepository;
-import com.libapp.backend.repository.UserRepository;
-import com.libapp.backend.security.JwtUtils;
-import com.libapp.backend.security.UserDetailsImpl;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,11 +12,18 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.libapp.backend.entity.Role;
+import com.libapp.backend.entity.RoleName;
+import com.libapp.backend.entity.User;
+import com.libapp.backend.repository.RoleRepository;
+import com.libapp.backend.repository.UserRepository;
+import com.libapp.backend.security.JwtUtils;
+import com.libapp.backend.security.UserDetailsImpl;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -73,15 +78,16 @@ public class AuthController {
         } else {
             strRoles.forEach(role -> {
                 switch (role) {
-                    case "admin":
+                    case "admin" -> {
                         Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
                                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
                         roles.add(adminRole);
-                        break;
-                    default:
+                    }
+                    default -> {
                         Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
                                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
                         roles.add(userRole);
+                    }
                 }
             });
         }
